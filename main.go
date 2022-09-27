f85f8ad1cefd938552eeff84557827e6f56e2ed0
gray-pigens <laowukeji@qq.com>
2022-09-27 22:19:53 +0800

    登录成功

--- main.go
+++ main.go
@@ -10,9 +10,7 @@ import (
 )
 
 func main() {
-
 	initDB()
-
 	router := gin.Default()
 	router.GET("/test", func(ctx *gin.Context) {
 		ctx.JSON(200, "嘿嘿嘿嘿")
@@ -21,13 +19,28 @@ func main() {
 	router.StaticFS("/FixHot", http.Dir("FixHot"))
 
 	router.POST("/login", login)
+	router.PUT("/register", register)
 	router.Run()
 }
 
 type User struct {
 	Id       int
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Username string
+	Password string
+}
+
+var rspState = make(map[string]string)
+
+func register(ctx *gin.Context) {
+	username := ctx.Request.FormValue("username")
+	password := ctx.Request.FormValue("password")
+	fmt.Println("register-------", username, password, rspState)
+
+	checkLength(username, password)
+	if rspState["state"] != "" {
+		ctx.JSON(401, rspState)
+		return
+	}
 }
 
 // 登录
@@ -35,45 +48,53 @@ func login(ctx *gin.Context) {
 
 	username := ctx.Request.FormValue("username")
 	password := ctx.Request.FormValue("password")
-	rspState := make(map[string]string)
 
-	fmt.Println("1-------", username, password, rspState)
+	fmt.Println("login-------", username, password, rspState)
 
-	if len(username) < 6 || len(username) > 16 {
-		rspState["state"] = "1001"
-		rspState["text"] = "用户名长度要在6-16位字符之间"
+	checkLength(username, password)
+	if rspState["state"] != "" {
 		ctx.JSON(401, rspState)
 		return
 	}
-
-	if len(password) < 6 || len(password) > 16 {
-		rspState["state"] = "1002"
-		rspState["text"] = "密码长度要在6-16位之间"
-		ctx.JSON(401, rspState)
-		return
-	}
-
 	//判断用户名是否存在
-	isExist := IsExist(username)
-	if isExist != nil {
+	isExist, err := IsExist(username)
+	if err != nil {
+		ctx.JSON(404, "出错了")
+		return
+	}
+	if isExist.Username != username {
 		rspState["state"] = "1003"
 		rspState["text"] = "用户名不存在"
 		ctx.JSON(401, rspState)
 		return
 	}
 
-	//判断用户名密码是否正确
-	isRight := IsRight(username, password)
-	if isRight != nil {
+	if isExist.Password != password {
 		rspState["state"] = "1003"
-		rspState["text"] = "用户名或密码错误"
+		rspState["text"] = "密码错误"
 		ctx.JSON(401, rspState)
 		return
 	}
 
+	fmt.Println(isExist.Username, isExist.Password)
 	rspState["state"] = "1004"
 	rspState["text"] = "登录成功"
 	ctx.JSON(200, rspState)
+}
+
+func checkLength(username string, password string) {
+	rspState["state"], rspState["text"] = "", ""
+	if len(username) < 6 || len(username) > 16 {
+		rspState["state"] = "1001"
+		rspState["text"] = "用户名长度要在6-16位字符之间"
+		return
+	}
+
+	if len(password) < 6 || len(password) > 16 {
+		rspState["state"] = "1002"
+		rspState["text"] = "密码长度要在6-16位之间"
+		return
+	}
 }
 
 //-------连接数据库----------------
@@ -99,7 +120,6 @@ func initDB() {
 		fmt.Println("mysql open is warn :", err)
 		return
 	}
-
 	//数据库设置
 	dbSQL.SetConnMaxLifetime(60)
 	dbSQL.SetConnMaxIdleTime(60)
@@ -116,18 +136,28 @@ func initDB() {
 	fmt.Println("mysql is connect success")
 }
 
-// 查看用户是否存在
-func IsExist(name string) error {
-	row, err := dbRead.Query("select * from user where name=?;", name)
-	fmt.Println("1----", row, err)
-	fmt.Println(row.Scan(name))
-	fmt.Println(err == nil)
-	return err
-}
+var (
+	id_          int
+	name_, pass_ string
+)
 
-// 查看用户密码是否正确
-func IsRight(name string, pass string) error {
-	row, err := dbRead.Query("select * from user where name=? and pass=?;", name, pass)
-	fmt.Println("2----", row, err)
-	return err
+// 查看用户和密码是否正确
+func IsExist(name string) (User, error) {
+	row, err := dbRead.Query("select * from user where name=?;", name)
+	if err != nil {
+		fmt.Println("查询用户出错:", err)
+		return User{}, err
+	}
+
+	for row.Next() {
+		err2 := row.Scan(&id_, &name_, &pass_)
+		if err2 != nil {
+			fmt.Println("解析用户名和密码发生错误:", err2)
+			return User{}, err2
+		}
+
+		fmt.Println("查找结果:", id_, name_, pass_)
+	}
+	defer row.Close()
+	return User{id_, name_, pass_}, nil
 }
